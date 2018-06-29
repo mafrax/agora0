6648ddadbc5d67f1dd6784b7cb34a7a095f42461
mmongin <marc.mongin@tobam.fr>
2018-06-29 16:53:31 +0200

    2906

--- Testserver/routes/index.js
+++ Testserver/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('register');
 });
+
+router.get('/test/', function(req, res, next) {
+	  res.render('profile');
+	});
+
+
+router.get('/test2/', function(req, res, next) {
+	  res.render('profile2');
+	});
 
 module.exports = router;
